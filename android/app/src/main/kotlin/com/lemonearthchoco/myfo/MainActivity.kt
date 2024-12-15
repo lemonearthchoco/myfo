ffb0faaea7a229cfca6df1c8dad13d9177d77354
chaeyeon <gkscodus11@gmail.com>
2024-12-15 23:15:35 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/lemonearthchoco/myfo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lemonearthchoco.myfo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/models/ObjectLog.dart
@@ -0,0 +1,18 @@
+class ObjectLog {
+  final String title;
+  final String subtitle;
+  final String description;
+  final List<String> yarns;
+  final List<String> needles;
+  final List<String> tags;
+  final List<String> gauges;
+
+  ObjectLog(
+      {required this.title,
+      required this.subtitle,
+      required this.description,
+      required this.yarns,
+      required this.needles,
+      required this.tags,
+      required this.gauges});
+}
